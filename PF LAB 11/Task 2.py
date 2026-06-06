eaa2045953975c57bbc966735c1b1dfc60431ef7
muhammadsohaib1234 <sohaibqaisrani46@gmail.com>
2026-06-06 21:09:40 +0500

    Create Task 2.py

---
+++ PF LAB 11/Task 2.py
@@ -0,0 +1,11 @@
+# TASK 2
+
+list=["fruit:", "mango", "fruit:", "apple", "vegetable:", "brinjal"]
+print(list)
+
+list=["name:muhammad sohaib",
+"age:18",
+"cast:qaisrani",
+"favtsub:programming fundamentals"]
+for i in list:
+    print(i)
