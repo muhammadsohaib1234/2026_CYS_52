c94b39761f09dd1eed8a3ace37612987819fcf9e
muhammadsohaib1234 <sohaibqaisrani46@gmail.com>
2026-06-06 18:23:18 +0500

    Add files via upload
    PF LAB 1

---
+++ Task 2.py
@@ -0,0 +1,10 @@
+def main():
+    name="SOHAIB"
+    age=18
+    city="Lahore"
+    print("Name:",name)
+    print("Age:",age)
+    print("City:",city)
+
+if __name__=="__main__":
+    main()
---
+++ Task 3.py
@@ -0,0 +1,25 @@
+
+def factorial(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    return fact
+
+
+def permutation(n, r):
+    return factorial(n) // factorial(n - r)
+
+def combination(n, r):
+    return factorial(n) // (factorial(r) * factorial(n - r))
+
+
+print("=== Permutation and Combination Calculator ===\n")
+
+n = int(input("Enter value of n: "))
+r = int(input("Enter value of r: "))
+
+if r > n or n < 0 or r < 0:
+    print("Error: r cannot be greater than n or negative values not allowed!")
+else:
+    print(f"\nPermutation P({n}, {r}) = {permutation(n, r)}")
+    print(f"Combination C({n}, {r}) = {combination(n, r)}")
