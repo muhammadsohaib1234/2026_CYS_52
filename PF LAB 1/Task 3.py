82947794b0e145b7e7e400b21ad07289d8b25570
muhammadsohaib1234 <sohaibqaisrani46@gmail.com>
2026-06-06 19:20:43 +0500

    Create Task 3.py

---
+++ PF LAB 1/Task 3.py
@@ -0,0 +1,12 @@
+# TASK 3
+
+def main():
+    name="SOHAIB"
+    age=18
+    city="Lahore"
+    print("Name:",name)
+    print("Age:",age)
+    print("City:",city)
+
+if __name__=="__main__":
+    main()
