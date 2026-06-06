2e8e20ca0c8f0ef51c7f66325da6da11e1f92da2
muhammadsohaib1234 <sohaibqaisrani46@gmail.com>
2026-06-06 19:18:46 +0500

    Add main function to print university details

---
+++ PF LAB 1/task 1.py
@@ -0,0 +1,9 @@
+# TASK 1
+def main():
+    uni="UET LAHORE"
+    city="Lahore"
+    department="Computer Engineering"
+    field="CYS"
+print(uni,city,department,field,sep="-")
+if __name__=="__main__":
+main()
