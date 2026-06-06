8691ad70abaf569b589b7dccebbcb1cb27512a53
muhammadsohaib1234 <sohaibqaisrani46@gmail.com>
2026-06-06 21:26:44 +0500

    Add Tasks 7 to 10 for set operations in Python

---
+++ PF LAB 12/Tasks 7-10.py
@@ -0,0 +1,25 @@
+#TASK 7
+k={2,3,4,5,6,7,8,9}
+k.pop()
+print(k)
+
+#TASK 8
+i1={"muhammad sohaib"}
+i2={"qaisrani"}
+i3={"dgk"}
+a=set.union(i1, i2,i3)
+print(a)
+
+#TASK 9
+i1={"muhammad sohaib"}
+i2={"qaisrani","dgk"}
+i3={"dgk"}
+a=set.union(i1, i2,i3)
+print(a)
+
+#TASK 10
+s1={1,2,3,"sohaib"}
+s2={4,5,6,"dgk"}
+s1.update(s2)
+print(s1)
+print(s2)
